9513583db658820a5064ef213495d70c972936a9
Borja Quevedo <borjota@gmail.com>
2017-10-26 16:00:40 +0200

    Working dagger setup

--- app/src/main/java/com/github/niltsiar/ultimatescrobbler/di/ApplicationComponent.java
+++ app/src/main/java/com/github/niltsiar/ultimatescrobbler/di/ApplicationComponent.java
@@ -1,17 +1,24 @@
 package com.github.niltsiar.ultimatescrobbler.di;
 
+import android.app.Application;
 import com.github.niltsiar.ultimatescrobbler.UltimateScrobblerApplication;
 import com.github.niltsiar.ultimatescrobbler.di.module.ActivityBindingModule;
 import com.github.niltsiar.ultimatescrobbler.di.module.ApplicationModule;
 import com.github.niltsiar.ultimatescrobbler.di.module.NetworkModule;
+import dagger.BindsInstance;
 import dagger.Component;
-import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
 @Component(modules = {ApplicationModule.class, ActivityBindingModule.class, AndroidSupportInjectionModule.class, NetworkModule.class})
-public interface ApplicationComponent extends AndroidInjector<UltimateScrobblerApplication> {
+public interface ApplicationComponent {
 
     @Component.Builder
-    abstract class Builder extends AndroidInjector.Builder<UltimateScrobblerApplication> {
+    interface Builder {
+        @BindsInstance
+        Builder application(Application application);
+
+        ApplicationComponent build();
     }
+
+    void inject(UltimateScrobblerApplication ultimateScrobblerApplication);
 }
--- app/src/main/java/com/github/niltsiar/ultimatescrobbler/di/module/NetworkModule.java
+++ app/src/main/java/com/github/niltsiar/ultimatescrobbler/di/module/NetworkModule.java
@@ -1,9 +1,33 @@
 package com.github.niltsiar.ultimatescrobbler.di.module;
 
+import com.github.niltsiar.ultimatescrobbler.remote.ScrobblerService;
+import com.serjltt.moshi.adapters.Wrapped;
+import com.squareup.moshi.Moshi;
 import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.moshi.MoshiConverterFactory;
 
 @Module
 public class NetworkModule {
 
+    @Provides
+    static ScrobblerService provideScrobblerService(OkHttpClient okHttpClient) {
+        Moshi moshi = new Moshi.Builder().add(Wrapped.ADAPTER_FACTORY)
+                                         .build();
 
+        return new Retrofit.Builder().baseUrl(ScrobblerService.WS_URL)
+                                     .client(okHttpClient)
+                                     .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                                     .addConverterFactory(MoshiConverterFactory.create(moshi))
+                                     .build()
+                                     .create(ScrobblerService.class);
+    }
+
+    @Provides
+    static OkHttpClient provideOkHttpClient() {
+        return new OkHttpClient();
+    }
 }
