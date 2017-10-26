cb0957d176268e073a162863cd24f8fc05cf256c
Borja Quevedo <borjota@gmail.com>
2017-10-26 19:22:04 +0200

    Create custom qualifiers for API key injection

---
+++ remote/src/main/java/com/github/niltsiar/ultimatescrobbler/remote/ApiKey.java
@@ -0,0 +1,10 @@
+package com.github.niltsiar.ultimatescrobbler.remote;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApiKey {
+}
