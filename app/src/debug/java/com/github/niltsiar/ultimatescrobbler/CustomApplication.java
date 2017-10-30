ab150550cf18ff363eb7b46618e47f2a75bf002d
Borja Quevedo <borjota@gmail.com>
2017-10-30 13:49:15 +0100

    Configure LeakCanary

--- app/src/debug/java/com/github/niltsiar/ultimatescrobbler/CustomApplication.java
+++ app/src/debug/java/com/github/niltsiar/ultimatescrobbler/CustomApplication.java
@@ -3,6 +3,7 @@ package com.github.niltsiar.ultimatescrobbler;
 import android.app.Application;
 import com.facebook.stetho.Stetho;
 import com.facebook.stetho.timber.StethoTree;
+import com.squareup.leakcanary.LeakCanary;
 import timber.log.Timber;
 
 public class CustomApplication extends Application {
@@ -10,6 +11,14 @@ public class CustomApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+
+        LeakCanary.install(this);
 
         Timber.plant(new Timber.DebugTree());
 
