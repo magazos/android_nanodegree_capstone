8b4f644279933e3493a514f7157158ddda8cbbb0
Borja Quevedo <borjota@gmail.com>
2017-11-06 23:49:26 +0100

    Implement savePlayedSong in ScrobblerDataRepository

--- data/src/main/java/com/github/niltsiar/ultimatescrobbler/data/ScrobblerDataRepository.java
+++ data/src/main/java/com/github/niltsiar/ultimatescrobbler/data/ScrobblerDataRepository.java
@@ -4,6 +4,7 @@ import com.github.niltsiar.ultimatescrobbler.data.mapper.CredentialsMapper;
 import com.github.niltsiar.ultimatescrobbler.data.mapper.PlayedSongMapper;
 import com.github.niltsiar.ultimatescrobbler.data.repository.ConfigurationCache;
 import com.github.niltsiar.ultimatescrobbler.data.repository.ScrobblerRemote;
+import com.github.niltsiar.ultimatescrobbler.data.repository.SongsCache;
 import com.github.niltsiar.ultimatescrobbler.domain.model.Credentials;
 import com.github.niltsiar.ultimatescrobbler.domain.model.PlayedSong;
 import com.github.niltsiar.ultimatescrobbler.domain.repository.ScrobblerRepository;
@@ -14,16 +15,18 @@ import javax.inject.Inject;
 public class ScrobblerDataRepository implements ScrobblerRepository {
 
     private ScrobblerRemote scrobblerRemote;
-    private ConfigurationCache configurationDataStore;
+    private ConfigurationCache configurationCache;
+    private SongsCache songsCache;
     private CredentialsMapper credentialsMapper;
     private PlayedSongMapper playedSongMapper;
 
     @Inject
-    public ScrobblerDataRepository(ScrobblerRemote scrobblerRemote, ConfigurationCache configurationDataStore,
+    public ScrobblerDataRepository(ScrobblerRemote scrobblerRemote, ConfigurationCache configurationCache, SongsCache songsCache,
             CredentialsMapper credentialsMapper,
             PlayedSongMapper playedSongMapper) {
         this.scrobblerRemote = scrobblerRemote;
-        this.configurationDataStore = configurationDataStore;
+        this.configurationCache = configurationCache;
+        this.songsCache = songsCache;
         this.credentialsMapper = credentialsMapper;
         this.playedSongMapper = playedSongMapper;
     }
@@ -31,11 +34,16 @@ public class ScrobblerDataRepository implements ScrobblerRepository {
     @Override
     public Single<String> requestMobileSessionToken(Credentials credentials) {
         return scrobblerRemote.requestMobileSessionToken(credentialsMapper.mapToEntity(credentials))
-                              .doOnSuccess(configurationDataStore.saveMobileSessionToken());
+                              .doOnSuccess(configurationCache.saveMobileSessionToken());
     }
 
     @Override
     public Completable sendNowPlaying(PlayedSong nowPlayingSong) {
         return scrobblerRemote.sendNowPlaying(playedSongMapper.mapToEntity(nowPlayingSong));
+    }
+
+    @Override
+    public Completable savePlayedSong(PlayedSong playedSong) {
+        return songsCache.savePlayedSong(playedSongMapper.mapToEntity(playedSong));
     }
 }
