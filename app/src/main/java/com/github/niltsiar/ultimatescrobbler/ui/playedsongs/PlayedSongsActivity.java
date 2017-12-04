99698055cc05a0ffb91abed98394f4d1f3501776
Borja Quevedo <borjota@gmail.com>
2017-12-04 17:51:34 +0100

    Creating scrobbled songs layout

--- app/src/main/java/com/github/niltsiar/ultimatescrobbler/ui/playedsongs/PlayedSongsActivity.java
+++ app/src/main/java/com/github/niltsiar/ultimatescrobbler/ui/playedsongs/PlayedSongsActivity.java
@@ -1,9 +1,7 @@
 package com.github.niltsiar.ultimatescrobbler.ui.playedsongs;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import com.github.niltsiar.ultimatescrobbler.R;
 
 public class PlayedSongsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -22,16 +19,6 @@ public class PlayedSongsActivity extends AppCompatActivity implements Navigation
         setContentView(R.layout.activity_played_songs);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .show();
-            }
-        });
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -74,23 +61,14 @@ public class PlayedSongsActivity extends AppCompatActivity implements Navigation
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_queue) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
