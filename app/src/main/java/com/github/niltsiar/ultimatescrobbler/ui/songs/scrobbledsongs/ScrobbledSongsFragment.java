19fdad61a334605f374394cd813ef86e64c32b36
Borja Quevedo <borjota@gmail.com>
2017-12-12 18:50:16 +0100

    Basic fragment structure

---
+++ app/src/main/java/com/github/niltsiar/ultimatescrobbler/ui/songs/scrobbledsongs/ScrobbledSongsFragment.java
@@ -0,0 +1,21 @@
+package com.github.niltsiar.ultimatescrobbler.ui.songs.scrobbledsongs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.github.niltsiar.ultimatescrobbler.R;
+
+public class ScrobbledSongsFragment extends Fragment {
+
+    public ScrobbledSongsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scrobbled_songs, container, false);
+    }
+}
