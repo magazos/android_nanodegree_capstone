6ac61d8bbb3a4eb25863147b19a1e8d87d6d3dd3
Borja Quevedo <borjota@gmail.com>
2017-12-28 18:45:01 +0100

    Creating configuration activity

---
+++ app/src/main/java/com/github/niltsiar/ultimatescrobbler/ui/configuration/ConfigurationActivity.java
@@ -0,0 +1,6 @@
+package com.github.niltsiar.ultimatescrobbler.ui.configuration;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ConfigurationActivity extends AppCompatActivity {
+}
