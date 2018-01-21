44dc747beceb680756c09752ee16406723ebae4c
Borja Quevedo <borjota@gmail.com>
2018-01-21 22:29:14 +0100

    Fix image loading

--- app/src/main/java/com/github/niltsiar/ultimatescrobbler/widget/UltimateScrobblerWidgetService.java
+++ app/src/main/java/com/github/niltsiar/ultimatescrobbler/widget/UltimateScrobblerWidgetService.java
@@ -1,10 +1,16 @@
 package com.github.niltsiar.ultimatescrobbler.widget;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Binder;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.text.TextUtils;
 import android.widget.AdapterView;
 import android.widget.RemoteViews;
@@ -13,7 +19,7 @@ import com.github.niltsiar.ultimatescrobbler.R;
 import com.github.niltsiar.ultimatescrobbler.cache.database.InfoSongColumns;
 import com.github.niltsiar.ultimatescrobbler.cache.database.SongsProvider;
 import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
+import java.io.IOException;
 
 public class UltimateScrobblerWidgetService extends RemoteViewsService {
 
@@ -67,28 +73,22 @@ public class UltimateScrobblerWidgetService extends RemoteViewsService {
 
                 RemoteViews view = new RemoteViews(getPackageName(), R.layout.widget_item);
                 view.setTextViewText(R.id.song, trackName);
+                Bitmap bitmap;
                 if (!TextUtils.isEmpty(albumArtUrl)) {
-                    Picasso.with(getApplicationContext())
-                           .load(albumArtUrl)
-                           .placeholder(R.drawable.ic_note)
-                           .error(R.drawable.ic_note)
-                           .into(new Target() {
-                               @Override
-                               public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                                   view.setImageViewBitmap(R.id.album_art, bitmap);
-                               }
+                    try {
+                        bitmap = Picasso.with(getApplicationContext())
+                                        .load(albumArtUrl)
+                                        .placeholder(R.drawable.ic_note)
+                                        .error(R.drawable.ic_note)
+                                        .get();
+                    } catch (IOException e) {
+                        bitmap = getBitmapFromVectorDrawable(getApplicationContext(), R.drawable.ic_note);
+                    }
 
-                               @Override
-                               public void onBitmapFailed(Drawable errorDrawable) {
-
-                               }
-
-                               @Override
-                               public void onPrepareLoad(Drawable placeHolderDrawable) {
-
-                               }
-                           });
+                } else {
+                    bitmap = getBitmapFromVectorDrawable(getApplicationContext(), R.drawable.ic_note);
                 }
+                view.setImageViewBitmap(R.id.album_art, bitmap);
 
                 return view;
             }
@@ -111,6 +111,20 @@ public class UltimateScrobblerWidgetService extends RemoteViewsService {
             @Override
             public boolean hasStableIds() {
                 return false;
+            }
+
+            private Bitmap getBitmapFromVectorDrawable(Context context, @DrawableRes int drawableId) {
+                Drawable drawable = ContextCompat.getDrawable(context, drawableId);
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                    drawable = (DrawableCompat.wrap(drawable)).mutate();
+                }
+
+                Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                drawable.draw(canvas);
+
+                return bitmap;
             }
         };
     }
